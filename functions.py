d340ad829b59e099cc2080ebe01d28820ebbbe36
Leonardo <lorlando@est.frba.utn.edu.ar>
2020-10-18 17:45:34 -0300

    tuples and functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+def say_hi():
+    print("Hi")
+
+def cube(number):
+    return number*number*number
+
+say_hi()
+print(cube(4))
---
+++ tuples.py
@@ -0,0 +1,4 @@
+#container where we can store different values
+#detalle importante: es inmutable, por lo que sentencias como coordinates[0] = 2, darán error
+coordinates = (4,5)
+print(coordinates[0])
